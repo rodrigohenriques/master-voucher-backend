04b642c4033c7928cc88e29f5e39227ad4c43653
Samir <samir.moreira@m4u.com.br>
2015-04-11 19:21:08 -0300

    fisrt version with payment ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+before_action do  
+  resource = controller_name.singularize.to_sym
+  method = "#{resource}_params"
+  params[resource] &&= send(method) if respond_to?(method, true)
+end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  #ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self)
 
 
   root 'payment#index'
 
-  get 'payment/index'
-  get 'payment/pay'
+  get 'payment/form'
+  post 'payment/pay'
+  get 'payment/token'
 
 
 end
